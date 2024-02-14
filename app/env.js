ae2fc598f345c5207db2f3e9f33ff732325eff3a
MeganHancock <mgnhnck@gmail.com>
2024-02-14 13:37:41 -0700

    axios and env set up

--- app/env.js
+++ app/env.js
@@ -1,5 +1,10 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
-export const domain = ''
-export const audience = ''
-export const clientId = ''
+export const domain = 'codeworksclassroom.auth0.com'
+export const audience = 'https://codeworksclassroom.com'
+export const clientId = 'pOXw2OGv1LsYi7LEBmDF04RLkXQvldml'
+
+
+// domain: codeworksclassroom.auth0.com
+// audience: https://codeworksclassroom.com
+// clientId: pOXw2OGv1LsYi7LEBmDF04RLkXQvldml
